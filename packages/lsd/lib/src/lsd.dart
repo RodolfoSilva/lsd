b7695e7edb7e34099d84bb2cedb2491d1512a404
Rodolfo Silva <contato@rodolfosilva.com>
2023-02-01 20:33:46 -0300

    add support to parse null cations and widgets

--- packages/lsd/lib/src/lsd.dart
+++ packages/lsd/lib/src/lsd.dart
@@ -22,10 +22,20 @@ class Lsd {
   LsdWidget parseWidget(Map<String, dynamic> element) =>
       widgetParser.fromJson(element);
 
+  LsdWidget? parseWidgetOrNull(dynamic element) {
+    if (element == null) return null;
+    return parseWidget(element);
+  }
+
   bool isWidget(dynamic element) => widgetParser.isWidget(element);
 
   LsdAction parseAction(Map<String, dynamic> element) =>
       actionParser.fromJson(element);
+
+  LsdAction? parseActionOrNull(dynamic element) {
+    if (element == null) return null;
+    return parseAction(element);
+  }
 
   bool isAction(dynamic element) => actionParser.isAction(element);
 
